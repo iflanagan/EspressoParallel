56098de5fa68df532851ddb591a077cd12085dfc
iflanagan <ianianf@yahoo.com>
2019-02-19 15:11:12 -0600

    Added more Files to project.

---
+++ scripts/espresso-real.sh
@@ -0,0 +1,1 @@
+java -jar ../runners/runner-1.5.jar espresso --apikey 3F1A3637B61D4786BC219A7519E69206 --app 51 --test ../apps/ImagineBanking-1.2-test.apk --device Samsung_Galaxy_S6_POC29 --datacenter EU
---
+++ scripts/xcui-real.sh
@@ -0,0 +1,1 @@
+java -jar runners/runner-1.5.jar config --path configs/espresso-parallel.yml --apikey AD2449B3C4B8423F9EC5BA8ECBD09CD5
