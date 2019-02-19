07844240e5f826e5815d58cb0a9bcb5ef9a9e51d
iflanagan <ianianf@yahoo.com>
2019-02-19 15:30:01 -0600

    fixed xcuitest script.

--- scripts/xcui-real.sh
+++ scripts/xcui-real.sh
@@ -1,1 +1,1 @@
-java -jar runners/runner-1.5.jar config --path configs/espresso-parallel.yml --apikey AD2449B3C4B8423F9EC5BA8ECBD09CD5
+java -jar runners/runner-1.5.jar config --path configs/conf_xcui --apikey ${API_KEY} 
